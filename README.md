110153a1798f28487119ccc55c51680a03554308
Sigal <ssax18@gmail.com>
2019-11-16 22:23:51 +0200

    Add initial step for retrieving user data - Add basic UI with hardcoded value - Sets up routing to get values from form as a JSON

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This project evokes the SkyScanner Flight API (with the help of [RapidAPI](https://rapidapi.com/) to check the lowest price available for travel between two locations for a specific date.
 
 To accomplish:
+- [x] Create basic backend that has initial hardcoded values, but receives correct data using the Skyscanner API
+- [x] Add functionality to accept user input using basic HTML
+- [ ] Build a front-end with Angular as the framework on-top of backend for user interaction
 - [ ] Create basic HTTP client in JavaScript to receive, consolidate, format, and package data before sending response
-- [ ] Add functionality to accept user input
-- [ ] Build a front-end on top for user interaction
--- app.js
+++ app.js
@@ -1,55 +1,90 @@
 var express = require('express');
+const app = express();
+const path = require('path');
+
 var bodyParser= require('body-parser');
-var unirest = require("unirest");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-var connDeets = require('./connDeets');
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-var req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/CUN-sky/MIA-sky/2019-09-01");
+app.use(bodyParser.urlencoded({ extended: true}))
 
-req.headers({
-	"x-rapidapi-host": connDeets.host,
-	"x-rapidapi-key": connDeets.api_key
-});
+// #############
+// Client
+// #############
 
-req.query({
-	"inboundpartialdate": "2019-12-01"
-});
+app.post('/submit', (req, res) => {
+  console.log(req.body)
+  const origin = req.body.origin
+  const destination = req.body.destination
+  const date = req.body.date
+  // res.redirect('/')
+})
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
+app.listen(3000, () => {
+  console.log(`App running at http://localhost:3000`)
+})
 
-	var queryMap = new Map ();
+// #############
+// API
+// #############
+// var unirest = require("unirest");
 
-	for (quote in res.body["Quotes"]) {
-		var price = res.body["Quotes"][quote]["MinPrice"]
-		console.log("Price to travel to MIA -> CUN ");
-		console.log(price);
-	}
+// var connDeets = require('./connDeets');
 
-	// GOAL: grab price of flight
-	var price = res.body["Quotes"][0]["MinPrice"]
-	// ---------------------
+// var req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/CUN-sky/MIA-sky/2019-01-12");
 
-	// GOAL: direct flight?
-	var isDirect = res.body["Quotes"][0]["Direct"]
-	// ---------------------
-
-	// GOAL: change number of id to name of flight company
-	// traverse carrierIds for outbound flights
-	var idToCarr;
-	for (var id in res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"]) {
-		// traverse all known carriers
-		for (var key in res.body["Carriers"]) {
-			// check if the carrierIds match the carriers provided and if so change their
-			if (res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id] == res.body["Carriers"][key]["CarrierId"]) {
-				res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id] = res.body["Carriers"][key]["Name"]
-				idToCarr = res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id];
-			}
-		}
-	}
-
-	queryMap.set(price, idToCarr)
-	for (var [key, value] of queryMap) {
-  	console.log(key + ' : ' + value);
-	}
-});
+// req.headers({
+// 	"x-rapidapi-host": connDeets.host,
+// 	"x-rapidapi-key": connDeets.api_key
+// });
+//
+// req.query({
+// 	"inboundpartialdate": "2019-01-12"
+// });
+//
+// req.end(function (res) {
+// 	// console.log(res)
+// 	if (res.error) {
+// 		console.log("One or more fields are invalid. Please check input and try again.");
+// 		process.exit(1)
+// 	}
+//
+// 	var queryMap = new Map ();
+//
+// 	for (quote in res.body["Quotes"]) {
+// 		var price = res.body["Quotes"][quote]["MinPrice"]
+// 		console.log("Price to travel to MIA -> CUN ");
+// 		// console.log(price);
+// 	}
+//
+// 	// GOAL: grab price of flight
+// 	var price = res.body["Quotes"][0]["MinPrice"]
+// 	// ---------------------
+//
+// 	// GOAL: direct flight?
+// 	var isDirect = res.body["Quotes"][0]["Direct"]
+// 	// ---------------------
+//
+// 	// GOAL: change number of id to name of flight company
+// 	// traverse carrierIds for outbound flights
+// 	var idToCarr;
+// 	for (var id in res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"]) {
+// 		// traverse all known carriers
+// 		for (var key in res.body["Carriers"]) {
+// 			// check if the carrierIds match the carriers provided and if so change their
+// 			if (res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id] == res.body["Carriers"][key]["CarrierId"]) {
+// 				res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id] = res.body["Carriers"][key]["Name"]
+// 				idToCarr = res.body["Quotes"][0]["OutboundLeg"]["CarrierIds"][id];
+// 			}
+// 		}
+// 	}
+//
+// 	queryMap.set(price, idToCarr)
+// 	for (var [key, value] of queryMap) {
+//   	// console.log(key + ' : ' + value);
+// 	}
+// });
